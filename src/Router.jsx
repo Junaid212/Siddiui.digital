ce8648c209736ad1d903d43c8292f92341eb7faa
Mohammed Junaid <mohadjunaid212@gmail.com>
2026-02-19 10:11:17 +0530

    nav bar1

--- src/Router.jsx
+++ src/Router.jsx
@@ -19,7 +19,7 @@ function AppRouters(){
     return(
         <Routes>
             
-            <Route path="/" element={<HomePage />} />
+            <Route path="/Siddiui.digital/" element={<HomePage />} />
             <Route path="about" element={<AboutPage />} />
             <Route path="services" element={<ServicePage />} />
             <Route path="e-book" element={<SingleServicePage />} />
